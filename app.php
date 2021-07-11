4aad9904709f414c062c759d20a0544a7711f860
DavegTheMighty <dave@example.com>
2021-07-11 11:59:59 +0100

    Pin Demo - Json Encode response body

--- app.php
+++ app.php
@@ -12,10 +12,12 @@ try {
     DB::init();
     $pin = (new PinController())->handleRequest();
     http_response_code(200);
-    echo $pin;
+    header('Content-type: application/json');
+    echo json_encode($pin);
 } catch (NotFoundException $e) {
     http_response_code(404);
-    echo "No valid and unique pin was found.";
+    header('Content-type: application/json');
+    echo json_encode("No valid and unique pin was found.");
 } catch (\Throwable $th) {
     http_response_code(500);
 } finally {
